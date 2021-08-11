c4950f75cd388d54a9e02aa454e398a5f46f6d89
payal Gajbhiye <payal.gajbhiye1998@gmail.com>
2021-08-11 13:28:55 +0530

    Addition of Two Numbers

---
+++ addition.js
@@ -0,0 +1,15 @@
+// Add two number with simple line of code
+const num1 = 10;
+const num2 = 20;
+
+const sum = num1 + num2;
+console.log(sum);
+
+//Add two number with function
+function add(num1, num2) {
+  const addition = num1 + num2;
+  return addition;
+}
+
+const result = add(100, 200);
+console.log(result);
